9a9069862bf8caa2044c2c5890909fc4c69ea13a
Aditya Chaturvedi <achatur755@gmail.com>
2024-05-20 14:53:54 +0530

    Video calling with screen share feature added

---
+++ client/WebRTC/README.md
@@ -0,0 +1,2 @@
+# Lets_get_interview_ready
+This is a interview prep video calling website where multiple people can connect at the same time with audio and video conferencing with real time chat and have practice for interviews and group discussions.
---
+++ client/WebRTC/js/room.js
@@ -0,0 +1,74 @@
+//whenever the message is typed it will automatically scroll to the bottom
+let messagesContainer = document.getElementById("messages");
+messagesContainer.scrollTop = messagesContainer.scrollHeight;
+
+//show and hide the members and chat
+const memberContainer = document.getElementById("members__container");
+const memberButton = document.getElementById("members__button");
+
+//show and hide the members and chat
+const chatContainer = document.getElementById("messages__container");
+const chatButton = document.getElementById("chat__button");
+
+let activeMemberContainer = false;
+
+memberButton.addEventListener("click", () => {
+  if (activeMemberContainer) {
+    memberContainer.style.display = "none";
+  } else {
+    memberContainer.style.display = "block";
+  }
+
+  activeMemberContainer = !activeMemberContainer;
+});
+
+let activeChatContainer = false;
+
+chatButton.addEventListener("click", () => {
+  if (activeChatContainer) {
+    chatContainer.style.display = "none";
+  } else {
+    chatContainer.style.display = "block";
+  }
+
+  activeChatContainer = !activeChatContainer;
+});
+
+let displayFrame = document.getElementById("stream__box");
+let videoFrames = document.getElementsByClassName("video__container");
+let userIdInDisplayFrame = null;
+
+//for expanding the main frame when a video frame is clicked
+let expandVideoFrame = (e) => {
+  let child = displayFrame.children[0];
+  if (child) {
+    document.getElementById("streams__container").append(child);
+  }
+  displayFrame.style.display = "block";
+  displayFrame.appendChild(e.currentTarget);
+  userIdInDisplayFrame = e.currentTarget.id;
+
+  for (let i = 0; i < videoFrames.length; i++) {
+    if (videoFrames[i].id != userIdInDisplayFrame) {
+      videoFrames[i].style.height = "100px";
+      videoFrames[i].style.width = "100px";
+    }
+  }
+};
+for (let i = 0; i < videoFrames.length; i++) {
+  videoFrames[i].addEventListener("click", expandVideoFrame);
+}
+
+let hideDisplayFrame = () => {
+  userIdInDisplayFrame = null;
+  displayFrame.style.display = null;
+
+  let child = displayFrame.children[0];
+  document.getElementById("streams__container").appendChild(child);
+  for (let i = 0; i < videoFrames.length; i++) {
+    videoFrames[i].style.height = "300px";
+    videoFrames[i].style.width = "300px";
+  }
+};
+
+displayFrame.addEventListener("click", hideDisplayFrame);
